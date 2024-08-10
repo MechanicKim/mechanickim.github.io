4b10e9436d6d610803cb1ee1823659eabbd93183
mechanic0688 <mechanic0688@MAC-MECHANIC0688.local>
2024-08-10 15:32:52 +0900

    feat: 페이지 개발환경 구축

---
+++ jason-lab/webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, 'src', 'index.tsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, '../'),
+  },
+  resolve: {
+		extensions: ['.js', 'ts', '.jsx', '.tsx'],
+	},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src', 'index.html'),
+		}),
+	],
+};
