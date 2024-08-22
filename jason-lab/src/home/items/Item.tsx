d7ec52cf20ca5670879d2bf7911e5264607d8bbc
mechanic0688 <mechanic0688@MAC-MECHANIC0688.local>
2024-08-22 17:57:22 +0900

    feat: 홈 로그 추가

--- jason-lab/src/home/items/Item.tsx
+++ jason-lab/src/home/items/Item.tsx
@@ -2,28 +2,28 @@ import React from 'react';
 
 import Spec from './Spec';
 
-import githubMark from '../imgs/github-mark.png';
+
 
 type TProps = {
   title: string;
   img: string;
   desc: string;
+  specs: string[];
+  LinkIcon: React.JSX.Element;
 };
 
-function Item({ title, img, desc }: TProps) {
+function Item({ title, img, desc, specs, LinkIcon }: TProps) {
   return (
     <div className="p-3 flex border-y">
       <img className="w-60 h-48 object-cover" src={img} />
       <div className="px-3 flex-1 flex flex-col">
         <h4 className="font-semibold text-xl text-gray-900">{title}</h4>
-        <Spec items={['Electron', 'React', 'Typescript', 'Tailwind']} />
+        <Spec items={specs} />
         <p className="mt-2 flex-1 text-gray-800">
           {desc}
         </p>
         <p className="flex justify-end">
-          <a href="https://github.com/MechanicKim/rss-news-reader/tree/main" target="_blank">
-            <img className="w-6" src={githubMark} />
-          </a>
+          {LinkIcon}
         </p>
       </div>
     </div>
