034f8d8f4f34df1848194ad70ce80b22c424c2fe
Jason <jason@Jasonui-MacBookAir.local>
2025-11-03 22:32:43 +0900

    feat: 인트로 영역 추가

--- jason-page/src/components/Header/index.tsx
+++ jason-page/src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>My Challenge</h1>
+      <h1>챌린지 로그</h1>
     </header>
   );
 }
